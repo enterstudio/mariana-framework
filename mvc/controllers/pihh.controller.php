fc7f136b930b037978556729da4faef248eb1cdc
Filipe S? <pihh.rocks@gmail.com>
2016-01-25 10:28:34 +0000

    added admin panel to snippets

---
+++ mvc/controllers/pihh.controller.php
@@ -0,0 +1,21 @@
+<?php
+/**
+  * Created with love using Mariana Framework
+  * Need help? Ask Pihh The Creator pihh.rocks@gmail.com
+  */
+
+use Mariana\Framework;
+use Mariana\Framework\Controller;
+use Mariana\Framework\Database;
+
+class Pihh extends Controller{
+
+    /**
+     * Default method;
+     */
+     public function index(){
+
+     }
+
+}
+?>
