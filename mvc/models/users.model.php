7db9bcaf5eb6bd8878576359fa407188af44d383
pihh <pihh.rocks@gmail.com>
2016-02-14 20:32:22 +0000

    added query

--- mvc/models/users.model.php
+++ mvc/models/users.model.php
@@ -10,8 +10,18 @@ class Users extends Model{
     public function contacts(){
         return $this->has("contacts" ,"content", 'user_id', $limit = false);
         /*
-SELECT * FROM ( SELECT * FROM `users`) AS `users` , ( SELECT `user_id`, GROUP_CONCAT(`content` SEPARATOR ',' ) FROM `comments`) AS `comments` WHERE `users`.`name` = 'pihh' AND `users`.`id` = `comments`.`user_id` 
+            SELECT * FROM
+                ( SELECT * FROM `users`) AS `users` ,
+                ( SELECT `user_id`, GROUP_CONCAT(`content` SEPARATOR ',' ) FROM `comments`) AS `comments`
+                WHERE `users`.`name` = 'pihh'
+                AND `users`.`id` = `comments`.`user_id`
 
+            Tradução:
+            SELECT * FROM
+                ( SELECT * FROM table1 ) as table1 ,
+                ( SELECT pivot , GROUP CONCAT(what SEPARATOR ',') FROM table2 ) AS table2
+                WHERE table1 clauses
+                AND table1.primary = table2.key
          */
     }
 
